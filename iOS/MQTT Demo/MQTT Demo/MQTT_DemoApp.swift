5393d3d8159c50699d684e73d62667db7f858d01
Roman Tolmachev <roman.tolmachev@ninjarmm.com>
2025-11-20 14:20:31 -0300

    Initial iOS project setup

---
+++ iOS/MQTT Demo/MQTT Demo/MQTT_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTT_DemoApp.swift
+//  MQTT Demo
+//
+//  Created by Roman Tolmachev on 20-11-25.
+//
+
+import SwiftUI
+
+@main
+struct MQTT_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
